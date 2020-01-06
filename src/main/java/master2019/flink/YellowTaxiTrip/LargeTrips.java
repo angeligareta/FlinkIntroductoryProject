607d60c73b3454f9629860c35356791ca7dce137
Angel Igareta <angel@igareta.com>
2020-01-06 01:33:26 +0000

    Simplified a tuple in Large trips

--- src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
@@ -4,8 +4,9 @@ import master2019.flink.YellowTaxiTrip.utils.DatasetReader;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple7;
+import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
@@ -64,41 +65,46 @@ public class LargeTrips {
                                 return false; // This only happen in windows
                             }
                         })
-                        // Add new variables, yearAndMonth to group by, day and startedTimeInSeconds that will be our timestamp
-                        // Output => VendorID, yearAndMonth, day, startedTimeInSeconds, number_trips, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count
-                        .flatMap((Tuple3<Integer, String, String> row, Collector<Tuple7<Integer, String, String, Long, Integer, String, String>> out) -> {
+                        // Add new variables, yearAndMonth and day to group by
+                        // Output => VendorID, yearAndMonth, day, number_trips, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count
+                        .flatMap((Tuple3<Integer, String, String> row, Collector<Tuple6<Integer, String, String, Integer, String, String>> out) -> {
                             final String[] dateSplitted = row.f1.trim().split("[ :-]");
                             if (dateSplitted.length == 6) {
                                 final String yearAndMonth = dateSplitted[0] + "-" + dateSplitted[1];
                                 final String day = dateSplitted[2];
-                                final long startedTimeInSeconds = DatasetReader.getTimeInSeconds(row.f1);
-                                out.collect(new Tuple7<>(row.f0, yearAndMonth, day, startedTimeInSeconds, 1, row.f1, row.f2));
+                                out.collect(new Tuple6<>(row.f0, yearAndMonth, day, 1, row.f1, row.f2));
                             }
                         })
-                        .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING, Types.LONG, Types.INT, Types.STRING, Types.STRING))
+                        .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING, Types.INT, Types.STRING, Types.STRING))
                         // Assign started time in seconds as timestamp
                         .assignTimestampsAndWatermarks(
-                                new AscendingTimestampExtractor<Tuple7<Integer, String, String, Long, Integer, String, String>>() {
+                                new AscendingTimestampExtractor<Tuple6<Integer, String, String, Integer, String, String>>() {
                                     @Override
-                                    public long extractAscendingTimestamp(final Tuple7<Integer, String, String, Long, Integer, String, String> element) {
-                                        return element.f3 * 1000;
+                                    public long extractAscendingTimestamp(final Tuple6<Integer, String, String, Integer, String, String> row) {
+                                        try {
+                                            final long startedTimeInSeconds = DatasetReader.getTimeInSeconds(row.f4);
+                                            return startedTimeInSeconds * 1000;
+                                        } catch (final Exception e) {
+                                            e.printStackTrace();
+                                            return 0;
+                                        }
                                     }
                                 }
                         )
-                        // Group by venorId, year-month and day
+                        // Group by vendorId, year-month and day
                         .keyBy(0, 1, 2)
                         // Make time window of 3 hours in seconds
                         .timeWindow(Time.hours(3))
                         // Add number of trips per window of three hours and take min of pick time and max of dropoff
-                        .sum(4)
+                        .sum(3)
                         // Filter vendorId with more than 5 trips per window
-                        .filter(row -> row.f4 >= 5)
+                        .filter(row -> row.f3 >= 5)
                         // Project only asked columns
-                        .project(0, 4, 5, 6);
+                        .project(0, 3, 4, 5);
 
 
         // Write to output file
-        //selected_trips_per_vendorID.writeAsCsv(outputPath, FileSystem.WriteMode.OVERWRITE);
+        selected_trips_per_vendorID.writeAsCsv(outputPath, FileSystem.WriteMode.OVERWRITE);
 
         // If verbose mode is activated execute is not necessary
         final Boolean verboseMode = true;
@@ -106,6 +112,7 @@ public class LargeTrips {
         // Show first 20 results sorted by vendorIDby console
         if (verboseMode) {
             try {
+                env.execute();
                 selected_trips_per_vendorID.print();
             } catch (final Exception e) {
                 e.printStackTrace();
