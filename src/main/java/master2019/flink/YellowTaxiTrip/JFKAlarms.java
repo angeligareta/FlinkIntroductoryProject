bf78b1a03c698f352c38365bfe7d6f11a38825b3
Angel Igareta <angel@igareta.com>
2020-01-11 22:34:00 +0000

    Ref: Fix JFKAlarms program

--- src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -1,17 +1,21 @@
 package master2019.flink.YellowTaxiTrip;
 
 import master2019.flink.YellowTaxiTrip.utils.DatasetReader;
-import org.apache.flink.api.common.operators.Order;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple5;
-import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+
+import java.util.Iterator;
 
 /**
  * In this class the JFK airport trips program has to be implemented.
@@ -23,9 +27,9 @@ import org.apache.flink.util.Collector;
  * JFK can be found in RatecodeID = 2
  */
 public class JFKAlarms {
-	
-	private final static String FILE_NAME = "jfkAlarms.csv";
-	
+
+    private final static String FILE_NAME = "jfkAlarms.csv";
+
     public static void main(final String[] args) {
         // Get params from command line
         final ParameterTool params = ParameterTool.fromArgs(args);
@@ -37,36 +41,62 @@ public class JFKAlarms {
             return;
         }
 
-        // Set up the execution environment
-        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // Row has the shape 0 => VendorID, 1 => tpep_pickup_datetime, 2 => tpep_dropoff_datetime, 3 => passenger_count, 4 => RatecodeID
-        final DataSet<Tuple5<Integer, String, String, Integer, Integer>> jfkAlarmsSubset = DatasetReader.getJFKAlarmsParameters(env, inputPath);
+        // ID, initDate, endDate, passengerCount, RateCodeID
+        final DataStream<Tuple5<Integer, String, String, Integer, Integer>> jfkAlarmsSubset = DatasetReader.getJFKAlarmsParameters(env, inputPath);
 
         // Get the trips in the expected output: (vendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count)
-        final DataSet<Tuple> selected_trips_per_vendorID = jfkAlarmsSubset
+        final DataStream<Tuple4<Integer, String, String, Integer>> selected_trips_per_vendorID = jfkAlarmsSubset
                 // First filter trips where destination is JFK and trips where there are two or more passengers
                 .filter(row -> (row.f4 == 2) && (row.f3 >= 2))
-                // Flat map to show trips per day. Output => VendorID, day-of-year, hour, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count
-                .flatMap((Tuple5<Integer, String, String, Integer, Integer> row, Collector<Tuple6<Integer, String, String, String, String, Integer>> out) -> {
-                    final String[] dateSplitted = row.f1.trim().split("[ :-]");
-                    if (dateSplitted.length == 6) {
-                        final String dayOfYear = dateSplitted[0] + "-" + dateSplitted[1] + "-" + dateSplitted[2];
-                        final String hour = dateSplitted[3];
-                        out.collect(new Tuple6<>(row.f0, dayOfYear, hour, row.f1, row.f2, row.f3));
-                    }
-                })
-                .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING, Types.STRING, Types.STRING, Types.INT))
-                // Group by vendorId, year-month-day and hour
-                .groupBy(0, 1, 2)
-                // sum passengerCount, getMin of pickup date and getMax of dropOff date
-                .sum(5).andMin(3).andMax(4)
-                // Only select vendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count for output
-                .project(0, 3, 4, 5).sortPartition(1, Order.ASCENDING);
+                // Assign trip start time as timestamp
+                .assignTimestampsAndWatermarks(
+                        new AscendingTimestampExtractor<Tuple5<Integer, String, String, Integer, Integer>>() {
+                            @Override
+                            public long extractAscendingTimestamp(final Tuple5<Integer, String, String, Integer, Integer> row) {
+                                return DatasetReader.dateStringToDate(row.f1).getTime();
+                            }
+                        }
+                )
+                .keyBy(0)
+                // Time window of 1 hour
+                .window(TumblingEventTimeWindows.of(Time.hours(1)))
+                // Get sum of passenger count, take pickup time from first element and dropoff time from last element
+                .apply(new WindowFunction<Tuple5<Integer, String, String, Integer, Integer>, Tuple4<Integer, String, String, Integer>, Tuple, TimeWindow>() {
+                    @Override
+                    public void apply(final Tuple key, final TimeWindow window, final Iterable<Tuple5<Integer, String, String, Integer, Integer>> input,
+                                      final Collector<Tuple4<Integer, String, String, Integer>> out) {
+                        final Iterator<Tuple5<Integer, String, String, Integer, Integer>> iterator = input.iterator();
+                        String firstPickupDate = null, lastDropoffDate = null;
+                        int vendorID = 0, totalPassengerCount = 0;
 
+                        // Boolean for detecting if the element is the first item to save vendorId and pickupdate
+                        boolean firstItem = true;
+                        while (iterator.hasNext()) {
+                            final Tuple5<Integer, String, String, Integer, Integer> next = iterator.next();
+
+                            // If first element save vendorId and pickupdate
+                            if (firstItem) {
+                                vendorID = next.f0;
+                                firstPickupDate = next.f1;
+                                firstItem = false;
+                            }
+
+                            // If last element save dropoff date
+                            if (!iterator.hasNext()) {
+                                lastDropoffDate = next.f2;
+                            }
+
+                            totalPassengerCount += next.f3;
+                        }
+
+                        out.collect(new Tuple4<>(vendorID, firstPickupDate, lastDropoffDate, totalPassengerCount));
+                    }
+                });
 
         // Write to output file
-        selected_trips_per_vendorID.writeAsCsv(outputPath+"/"+FILE_NAME, FileSystem.WriteMode.OVERWRITE);
+        selected_trips_per_vendorID.writeAsCsv(outputPath + "/" + FILE_NAME, FileSystem.WriteMode.OVERWRITE);
 
         // If verbose mode is activated execute is not necessary
         final Boolean verboseMode = false;
@@ -74,7 +104,7 @@ public class JFKAlarms {
         // Show first 20 results sorted by vendorIDby console
         if (verboseMode) {
             try {
-                selected_trips_per_vendorID.sortPartition(0, Order.ASCENDING).first(20).print();
+                selected_trips_per_vendorID.print();
             } catch (final Exception e) {
                 e.printStackTrace();
             }
@@ -85,6 +115,5 @@ public class JFKAlarms {
                 e.printStackTrace();
             }
         }
-
     }
 }
--- src/main/java/master2019/flink/YellowTaxiTrip/utils/DatasetReader.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/utils/DatasetReader.java
@@ -74,8 +74,25 @@ public class DatasetReader {
      * @param filePath File where the dataset is
      * @return Tuple with shape 0 => VendorID, 1 => tpep_pickup_datetime, 2 => tpep_dropoff_datetime, 3 => passenger_count, 4 => RatecodeID
      */
-    public static DataSet<Tuple5<Integer, String, String, Integer, Integer>> getJFKAlarmsParameters(final ExecutionEnvironment env, final String filePath) {
-        return readFullDataset(env, filePath).project(0, 1, 2, 3, 5);
+//    public static DataSet<Tuple5<Integer, String, String, Integer, Integer>> getJFKAlarmsParameters(final ExecutionEnvironment env, final String filePath) {
+//        return readFullDataset(env, filePath).project(0, 1, 2, 3, 5);
+//    }
+
+    /**
+     * Returns the necessary data for the jfk alarms exercise.
+     *
+     * @param env      flink environment
+     * @param filePath File where the dataset is
+     * @return Tuple with shape 0 => VendorID, 1 => tpep_pickup_datetime, 2 => tpep_dropoff_datetime, 3 => passenger_count, 4 => RatecodeID
+     */
+    public static DataStream<Tuple5<Integer, String, String, Integer, Integer>> getJFKAlarmsParameters(final StreamExecutionEnvironment env, final String filePath) {
+        return env
+                .readTextFile(filePath)
+                .flatMap((String input, Collector<Tuple5<Integer, String, String, Integer, Integer>> collector) -> {
+                    String[] row = input.split(",");
+                    collector.collect(new Tuple5<>(Integer.parseInt(row[0]), row[1], row[2], Integer.parseInt(row[3]), Integer.parseInt(row[5])));
+                })
+                .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING, Types.INT, Types.INT));
     }
 
     /**
